73aea3b58ce8b20367d6d7f3b45391dac38c200f
Vikram Singh Rajpurohit <vrajpuro@its.jnj.com>
2020-04-05 14:09:08 +0530

    react phone store first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <p className="p-2 mt-2">
+        This is a demo react site, created by{" "}
+        <strong>Vikram Singh Rajpurohit</strong>
+      </p>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
+
+const FooterContainer = styled.section`
+  background: #b7e9f7;
+  position: fixed;
+  border: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  p {
+    text-align: center;
+  }
+`;
